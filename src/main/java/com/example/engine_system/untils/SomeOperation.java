9560ec214f5d34333f8c8c9e2c44a3f9072b38c2
chenwangnatsukashii <390827590.com>
2021-12-07 20:42:16 +0800

    WarningInfo 新增提醒信息常量实体 SomeOperation 对于一些常规操作封在一个实体中

---
+++ src/main/java/com/example/engine_system/untils/SomeOperation.java
@@ -0,0 +1,13 @@
+package com.example.engine_system.untils;
+
+/**
+ * @author shushu
+ * @date 2021/12/6
+ * @apiNote
+ */
+public class SomeOperation {
+
+    public static boolean isEmpty(String s) {
+        return s == null || "".equals(s.trim());
+    }
+}
