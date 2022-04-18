b3d1bf87979f0bba2e27f638843c61de3d5d6109
chenwangnatsukashii <390827590@qq.com>
2022-04-18 19:54:54 +0800

    添加lombok,删除getter，setter

--- src/main/java/com/example/engine_system/entity/ResumeEight.java
+++ src/main/java/com/example/engine_system/entity/ResumeEight.java
@@ -1,43 +1,35 @@
 package com.example.engine_system.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ResumeEight {
+@Data
+@NoArgsConstructor
+public class ResumeEight implements Serializable {
+
+    private static final long serialVersionUID = -644972534331302258L;
+
     private Integer id;
-
     private Integer planeId;
-
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date eightDate;
-
     private Integer todayStartTimes;
-
     private String groundFly;
-
     private String groundRated;
-
     private String groundTotal;
-
     private String airFly;
-
     private String airRated;
-
     private String airTotal;
-
     private String groundAirFly;
-
     private String groundAirRated;
-
     private String groundAirTotal;
-
     private String totalFly;
-
     private String totalRated;
-
     private String totalTotal;
-
     private Integer totalStartTimes;
 
     public ResumeEight(Integer id, Integer planeId, Date eightDate, Integer todayStartTimes, String groundFly, String groundRated, String groundTotal, String airFly, String airRated, String airTotal, String groundAirFly, String groundAirRated, String groundAirTotal, String totalFly, String totalRated, String totalTotal, Integer totalStartTimes) {
@@ -60,143 +52,4 @@ public class ResumeEight {
         this.totalStartTimes = totalStartTimes;
     }
 
-    public ResumeEight() {
-        super();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPlaneId() {
-        return planeId;
-    }
-
-    public void setPlaneId(Integer planeId) {
-        this.planeId = planeId;
-    }
-
-    public Date getEightDate() {
-        return eightDate;
-    }
-
-    public void setEightDate(Date eightDate) {
-        this.eightDate = eightDate;
-    }
-
-    public Integer getTodayStartTimes() {
-        return todayStartTimes;
-    }
-
-    public void setTodayStartTimes(Integer todayStartTimes) {
-        this.todayStartTimes = todayStartTimes;
-    }
-
-    public String getGroundFly() {
-        return groundFly;
-    }
-
-    public void setGroundFly(String groundFly) {
-        this.groundFly = groundFly == null ? null : groundFly.trim();
-    }
-
-    public String getGroundRated() {
-        return groundRated;
-    }
-
-    public void setGroundRated(String groundRated) {
-        this.groundRated = groundRated == null ? null : groundRated.trim();
-    }
-
-    public String getGroundTotal() {
-        return groundTotal;
-    }
-
-    public void setGroundTotal(String groundTotal) {
-        this.groundTotal = groundTotal == null ? null : groundTotal.trim();
-    }
-
-    public String getAirFly() {
-        return airFly;
-    }
-
-    public void setAirFly(String airFly) {
-        this.airFly = airFly == null ? null : airFly.trim();
-    }
-
-    public String getAirRated() {
-        return airRated;
-    }
-
-    public void setAirRated(String airRated) {
-        this.airRated = airRated == null ? null : airRated.trim();
-    }
-
-    public String getAirTotal() {
-        return airTotal;
-    }
-
-    public void setAirTotal(String airTotal) {
-        this.airTotal = airTotal == null ? null : airTotal.trim();
-    }
-
-    public String getGroundAirFly() {
-        return groundAirFly;
-    }
-
-    public void setGroundAirFly(String groundAirFly) {
-        this.groundAirFly = groundAirFly == null ? null : groundAirFly.trim();
-    }
-
-    public String getGroundAirRated() {
-        return groundAirRated;
-    }
-
-    public void setGroundAirRated(String groundAirRated) {
-        this.groundAirRated = groundAirRated == null ? null : groundAirRated.trim();
-    }
-
-    public String getGroundAirTotal() {
-        return groundAirTotal;
-    }
-
-    public void setGroundAirTotal(String groundAirTotal) {
-        this.groundAirTotal = groundAirTotal == null ? null : groundAirTotal.trim();
-    }
-
-    public String getTotalFly() {
-        return totalFly;
-    }
-
-    public void setTotalFly(String totalFly) {
-        this.totalFly = totalFly == null ? null : totalFly.trim();
-    }
-
-    public String getTotalRated() {
-        return totalRated;
-    }
-
-    public void setTotalRated(String totalRated) {
-        this.totalRated = totalRated == null ? null : totalRated.trim();
-    }
-
-    public String getTotalTotal() {
-        return totalTotal;
-    }
-
-    public void setTotalTotal(String totalTotal) {
-        this.totalTotal = totalTotal == null ? null : totalTotal.trim();
-    }
-
-    public Integer getTotalStartTimes() {
-        return totalStartTimes;
-    }
-
-    public void setTotalStartTimes(Integer totalStartTimes) {
-        this.totalStartTimes = totalStartTimes;
-    }
 }
--- src/main/java/com/example/engine_system/entity/ResumeNine.java
+++ src/main/java/com/example/engine_system/entity/ResumeNine.java
@@ -1,10 +1,18 @@
 package com.example.engine_system.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ResumeNine {
+@Data
+@NoArgsConstructor
+public class ResumeNine implements Serializable {
+
+    private static final long serialVersionUID = -2732055001081549592L;
+
     private Integer id;
     private Integer planeId;
 
@@ -47,151 +55,4 @@ public class ResumeNine {
         this.startTime = startTime;
     }
 
-    public ResumeNine() {
-        super();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPlaneId() {
-        return planeId;
-    }
-
-    public void setPlaneId(Integer planeId) {
-        this.planeId = planeId;
-    }
-
-    public Date getNineDate() {
-        return nineDate;
-    }
-
-    public void setNineDate(Date nineDate) {
-        this.nineDate = nineDate;
-    }
-
-    public Integer getGroundAllHour() {
-        return groundAllHour;
-    }
-
-    public void setGroundAllHour(Integer groundAllHour) {
-        this.groundAllHour = groundAllHour;
-    }
-
-    public Integer getGroundAllMinute() {
-        return groundAllMinute;
-    }
-
-    public void setGroundAllMinute(Integer groundAllMinute) {
-        this.groundAllMinute = groundAllMinute;
-    }
-
-    public Integer getGroundBigHour() {
-        return groundBigHour;
-    }
-
-    public void setGroundBigHour(Integer groundBigHour) {
-        this.groundBigHour = groundBigHour;
-    }
-
-    public Integer getGroundBigMinute() {
-        return groundBigMinute;
-    }
-
-    public void setGroundBigMinute(Integer groundBigMinute) {
-        this.groundBigMinute = groundBigMinute;
-    }
-
-    public Integer getGroundAddHour() {
-        return groundAddHour;
-    }
-
-    public void setGroundAddHour(Integer groundAddHour) {
-        this.groundAddHour = groundAddHour;
-    }
-
-    public Integer getGroundAddMinute() {
-        return groundAddMinute;
-    }
-
-    public void setGroundAddMinute(Integer groundAddMinute) {
-        this.groundAddMinute = groundAddMinute;
-    }
-
-    public Integer getAirAllHour() {
-        return airAllHour;
-    }
-
-    public void setAirAllHour(Integer airAllHour) {
-        this.airAllHour = airAllHour;
-    }
-
-    public Integer getAirAllMinute() {
-        return airAllMinute;
-    }
-
-    public void setAirAllMinute(Integer airAllMinute) {
-        this.airAllMinute = airAllMinute;
-    }
-
-    public Integer getAirBigHour() {
-        return airBigHour;
-    }
-
-    public void setAirBigHour(Integer airBigHour) {
-        this.airBigHour = airBigHour;
-    }
-
-    public Integer getAirBigMinute() {
-        return airBigMinute;
-    }
-
-    public void setAirBigMinute(Integer airBigMinute) {
-        this.airBigMinute = airBigMinute;
-    }
-
-    public Integer getAirAddHour() {
-        return airAddHour;
-    }
-
-    public void setAirAddHour(Integer airAddHour) {
-        this.airAddHour = airAddHour;
-    }
-
-    public Integer getAirAddMinute() {
-        return airAddMinute;
-    }
-
-    public void setAirAddMinute(Integer airAddMinute) {
-        this.airAddMinute = airAddMinute;
-    }
-
-    public Integer getTotalTimeHour() {
-        return totalTimeHour;
-    }
-
-    public void setTotalTimeHour(Integer totalTimeHour) {
-        this.totalTimeHour = totalTimeHour;
-    }
-
-    public Integer getTotalTimeMinute() {
-        return totalTimeMinute;
-    }
-
-    public void setTotalTimeMinute(Integer totalTimeMinute) {
-        this.totalTimeMinute = totalTimeMinute;
-    }
-
-    public Integer getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Integer startTime) {
-        this.startTime = startTime;
-    }
 }
