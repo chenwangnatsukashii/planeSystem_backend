f2fdfe45cb68e83b42f57438ce845a12165aeb91
chenwangnatsukashii <390827590@qq.com>
2022-04-21 14:14:48 +0800

    简化代码lombok

--- src/main/java/com/example/engine_system/model/User.java
+++ src/main/java/com/example/engine_system/model/User.java
@@ -1,57 +1,23 @@
 package com.example.engine_system.model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1978931913474202083L;
+
     private String userName;
     private String password;
     private String avatar;
     private String name;
     private String userId;
 
-    public User(String userName, String password, String avatar, String name, String userId) {
-        this.userName = userName;
-        this.password = password;
-        this.avatar = avatar;
-        this.name = name;
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
